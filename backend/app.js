5b6356f63f181468b90bfdf66677b1399cad75e6
Fabienholert <fabienholert@gmail.com>
2025-02-15 10:15:05 +0100

    mise en place des routes pour app

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,18 +11,49 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/books', (req, res, next) => {
+app.get('/api/books', (req, res, next) => {
     res.status(200).json();
+    next();
   });
 
-app.use('/api/books/:id', (req, res, next) => {
+app.get('/api/books/:id', (req, res, next) => {
     res.status(200).json();
+    next();
   });
 
-app.use('/api/books/bestrating', (req, res, next) => {
+app.get('/api/books/bestrating', (req, res, next) => {
     res.status(200).json();
+    next();
   });
 
+app.post ('/api/auth/signup', (req, res, next) => {
+    res.status(200).json();
+    next();
+  });
 
+  app.post ('/api/auth/login', (req, res, next) => {
+    res.status(200).json();
+    next();
+  });
+
+  app.post ('/api/books', (req, res, next) => {
+    res.status(200).json();
+    next();
+  });
+
+  app.post ('/api/books/:id/rating', (req, res, next) => {
+    res.status(200).json();
+    next();
+  });
+
+  app.put('/api/books/:id', (req, res, next) => {
+    res.status(200).json();
+    next();
+  });
+
+  app.delete('/api/books/:id', (req, res) => {
+    res.status(200).json();
+    next();
+  });
 
 module.exports = app;
